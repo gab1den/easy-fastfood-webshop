d982094022909218ecbfd0cbcbf39d2a2391da70
Maxim <anppvc@gmail.com>
2024-04-14 15:17:12 +0300

    Adding image

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
+
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('busket', views.busket, name='busket'),
     path('add_to_basket/<int:product_id>/', views.add_to_basket, name='add_to_basket'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
